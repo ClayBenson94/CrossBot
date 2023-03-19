728e27b8f7b5ada558df325f1cb88fdfdadbd69f
Clay Benson <clay.benson@oddball.io>
2023-03-19 19:58:44 -0400

    Add an HTTP server beacuse google cloud run wants one (#3)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-// Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const http = require('http')
+
 const token = process.env.DISCORD_BOT_TOKEN;
 
 // Import helper for registering slash commands
@@ -50,3 +51,13 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
+// I didn't realize, but apparently Google Cloud Run _requires_ responses to HTTP calls, so this dumb server is here to satisfy the contract
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("Hello, random person on the internet!");
+};
+
+const server = http.createServer(requestListener);
+server.listen(8080, '0.0.0.0', () => {
+    console.log(`Server is running!`);
+});

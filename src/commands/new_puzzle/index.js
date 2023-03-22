ef32aef6d8498923cd9a830c082e664633dfc4af
Clay Benson <clay.benson@oddball.io>
2023-03-21 20:35:56 -0400

    New puzz functionality (#4)

--- src/commands/new_puzzle/index.js
+++ src/commands/new_puzzle/index.js
@@ -1,4 +1,9 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
+const { generateSlug } = require('random-word-slugs')
+
+const ACTIVE_PUZZLES_CHANNEL_CATEGORY_ID="1084525753695215787"
+const PUZZ_WATCHERS_ROLE_ID="1086705029316087908"
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,7 +14,30 @@ module.exports = {
 			.setDescription('The full URL of the downforacross session')
 			.setRequired(true)),
 	async execute(interaction) {
+		const DFAC_REGEX = /https:\/\/downforacross.com\/beta\/game\/(.*)/igm //create this each function so that .test() doesn't mess up lastindex
+		await interaction.deferReply({ephemeral: true});
 		const url = interaction.options.getString('url');
-		await interaction.reply({content: `You sent ${url} as the URL!`, ephemeral: true});
+
+		if (!DFAC_REGEX.test(url)) {
+			await interaction.editReply({
+				content: "⚠️ Your URL didn't seem to be quite what I expected.\nMake sure it's got \"/beta/game\" in it to ensure it's a puzzle session!",
+				ephemeral: true
+			})
+			return
+		}
+
+		const channelName = generateSlug(3, {
+			format: 'kebab',
+			partsOfSpeech: ['adjective', 'adjective', 'noun']
+		}).replaceAll('-', '_');
+		const createdChannel = await interaction.member.guild.channels.create({
+			name: channelName,
+			type: ChannelType.GuildText,
+			topic: url
+		});
+		await createdChannel.setParent(ACTIVE_PUZZLES_CHANNEL_CATEGORY_ID);
+		await createdChannel.send(`🧩🚨 <@&${PUZZ_WATCHERS_ROLE_ID}>\nNew Puzzle alert!\n\nHead on over to ${url} to play along!`)
+		await createdChannel.setTopic(`Head on over to ${url} to play along!`)
+		await interaction.editReply({content: `#${createdChannel.id} has been created! 🎉`, ephemeral: true});
 	},
 };

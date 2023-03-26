0fa8d38653b2ac4bfc8571b7664a671bea1ce2b7
ClayBenson94 <clay.benson@oddball.io>
2023-03-25 22:22:50 -0400

    Subcommands for puzzles

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,6 +1,5 @@
 // You must export any commands from this file in order for them to be parsed by the client
 
 module.exports = {
- 	newpuzzle: require('./new_puzzle'),
- 	closepuzzle: require('./close_puzzle'),
+ 	puzzle: require('./puzzle'),
 }
